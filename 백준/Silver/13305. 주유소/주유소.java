26ee4fe847611f06c54277f72d35e5466753c52b
jihoon-gh <81089568+jihoon-gh@users.noreply.github.com>
2023-01-12 22:02:55 +0900

    [Silver III] Title: 주유소, Time: 340 ms, Memory: 33780 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] ways = new int[n-1];
+        int[] cities = new int[n];
+
+        String[] splits = br.readLine().split(" ");
+        for(int i = 0; i < n-1; i++){
+            ways[i] = Integer.parseInt(splits[i]);
+        }
+
+        splits = br.readLine().split(" ");
+        for(int i = 0; i < n; i++){
+            cities[i] = Integer.parseInt(splits[i]);
+        }
+
+        int price = 2000000000;
+        int result = 0;
+        for(int i = 0 ; i < n-1; i++)
+        {
+            price = Math.min(price, cities[i]);
+            result += price * ways[i];
+        }
+        System.out.println(result);
+    }
+}
